364c9f9542f7ddd85a40a41d2c8807d1c5ef4610
Fiona Voss <fionavoss@gmail.com>
2018-04-21 08:23:12 -0400

    Generate project

---
+++ lib/elixir_backalley_scorekeeper.ex
@@ -0,0 +1,18 @@
+defmodule ElixirBackalleyScorekeeper do
+  @moduledoc """
+  Documentation for ElixirBackalleyScorekeeper.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirBackalleyScorekeeper.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_backalley_scorekeeper_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirBackalleyScorekeeperTest do
+  use ExUnit.Case
+  doctest ElixirBackalleyScorekeeper
+
+  test "greets the world" do
+    assert ElixirBackalleyScorekeeper.hello() == :world
+  end
+end
